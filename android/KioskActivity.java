122cdda46f6b00d9b0d40e86cf941549659f7978
Javi Agut <JaviAgutB@gmail.com>
2017-11-30 17:27:27 +0100

    Now the status bar can not be displayed

--- android/KioskActivity.java
+++ android/KioskActivity.java
@@ -18,6 +18,13 @@ import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.graphics.PixelFormat;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
 public class KioskActivity extends CordovaActivity {
     
     public static volatile boolean running = false;
@@ -48,6 +55,8 @@ public class KioskActivity extends CordovaActivity {
         // https://github.com/apache/cordova-plugin-statusbar/blob/master/src/android/StatusBar.java
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        preventStatusBarExpansion(getApplicationContext());
     }
     
     @Override
@@ -66,8 +75,8 @@ public class KioskActivity extends CordovaActivity {
             Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
             sendBroadcast(closeDialog);
             
-            ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
-            am.moveTaskToFront(getTaskId(), ActivityManager.MOVE_TASK_WITH_HOME);
+            // ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+            // am.moveTaskToFront(getTaskId(), ActivityManager.MOVE_TASK_WITH_HOME);
             
             // sometime required to close opened notification area
             Timer timer = new Timer();
@@ -79,5 +88,46 @@ public class KioskActivity extends CordovaActivity {
             }, 500); // 0.5 second
         }
     }
-}
 
+    public static void preventStatusBarExpansion(Context context) {
+        WindowManager manager = ((WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE));
+
+        WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
+        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+        localLayoutParams.gravity = Gravity.TOP;
+        localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL|WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+
+        localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+
+        int resId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        int result = 0;
+        if (resId > 0) {
+            result = context.getResources().getDimensionPixelSize(resId);
+        } else {
+            // Use Fallback size:
+            result = 60; // 60px Fallback
+        }
+
+        localLayoutParams.height = result;
+        localLayoutParams.format = PixelFormat.TRANSPARENT;
+
+        CustomViewGroup view = new CustomViewGroup(context);
+        manager.addView(view, localLayoutParams);
+    }
+
+    public static class CustomViewGroup extends ViewGroup {
+        public CustomViewGroup(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(MotionEvent ev) {
+            // Intercepted touch!
+            return true;
+        }
+    }
+}
