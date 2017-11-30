a2d4a20be908a6d0ec943149b553cd1f4f7d2ead
Javi <javicharlie14@gmail.com>
2017-11-30 16:46:43 +0100

    Enable opening other applications

--- android/KioskActivity.java
+++ android/KioskActivity.java
@@ -66,8 +66,8 @@ public class KioskActivity extends CordovaActivity {
             Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
             sendBroadcast(closeDialog);
             
-            ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
-            am.moveTaskToFront(getTaskId(), ActivityManager.MOVE_TASK_WITH_HOME);
+            // ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+            // am.moveTaskToFront(getTaskId(), ActivityManager.MOVE_TASK_WITH_HOME);
             
             // sometime required to close opened notification area
             Timer timer = new Timer();
